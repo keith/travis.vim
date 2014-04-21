2b130a9879a2809fa24ad144be7d10e5113c14b2
Keith Smiley <keithbsmiley@gmail.com>
2014-04-20 20:58:19 -0400

    Cleanup date methods

--- travis/travisresponse.py
+++ travis/travisresponse.py
@@ -1,4 +1,3 @@
-from dateexception import DateException
 import datetime
 
 
@@ -15,33 +14,34 @@ class TravisResponse(object):
         self._end_time = branch["finished_at"]
         self._state = branch["state"]
 
-    @property
-    def build_id(self):
-        return self._build_id
-
-    @property
-    def start_time(self):
-        # TODO: Pretty this
-        return self._start_time
-
-    @property
-    def end_time(self):
-        return self._end_time
-
-    @property
-    def state(self):
-        # TODO: Make this a enum type thingy
-        return self._state
-
     def message(self):
         pass
 
-    @staticmethod
     def html_url(self, repo_url):
         return "https://travis-ci.org/%s/builds/%s" % (repo_url, self.build_id)
 
+    @property
+    def start_time(self):
+        try:
+            time = self.iso_date(self._start_time)
+            return TravisResponse.pretty_date(time)
+        except ValueError as e:
+            return e
+
+    @property
+    def end_time(self):
+        try:
+            time = self.iso_date(self._end_time)
+            return TravisResponse.pretty_date(time)
+        except ValueError:
+            return self.start_time
+
+    @property
+    def state(self):
+        return self._state.title()
+
     @staticmethod
-    def iso_date(self, dt):
+    def iso_date(dt):
         if not dt:
             return None
         return datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%SZ")
@@ -49,7 +49,7 @@ class TravisResponse(object):
     @staticmethod
     def pretty_date(d):
         if not d:
-            raise DateException("Invalid Date")
+            raise ValueError("Invalid Date")
 
         # TODO: Times are lying
         diff = datetime.datetime.utcnow() - d
